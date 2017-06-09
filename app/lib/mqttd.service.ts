2bccdf0bf0ab95a774e8b28de84a93f2b309fbc1
Alexandre Piel <alex@vps195721.ovh.net>
2017-06-09 18:46:22 +0200

    client list on connect

--- app/lib/mqttd.service.ts
+++ app/lib/mqttd.service.ts
@@ -28,7 +28,7 @@ export default class {
 
     ready(callback: Function) {
         this.mqttd.on('ready', callback);
-        this.mqttd.on('clientConnected', (client: any) => console.log('Client Connected:', client.id));
+        this.mqttd.on('clientConnected', (client: any) => console.log('Client Connected:', client.id, Object.keys(this.mqttd.clients)));
         this.mqttd.on('subscribed', (topic: string, client: any) => console.log('Client subscribed:', topic, client.id));
         return this;
     }
