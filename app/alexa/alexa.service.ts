297c734d48fe28d0758134ec883e77ce0f32f6d0
alex <alexandre.piel@gmail.com>
2017-08-08 19:52:47 +0200

    new actions

--- app/alexa/alexa.service.ts
+++ app/alexa/alexa.service.ts
@@ -4,7 +4,7 @@ import * as restify from 'restify';
 
 export default class {
     constructor(private mqttClient: Client) {}
-    
+
     call(body: any, request: restify.Request) {       
         console.log('Alexa');  
         console.log(JSON.stringify(body.request, null, 4));
@@ -12,8 +12,34 @@ export default class {
 
         let response: string = 'Ok';
         if (request.query && request.query.topic && request.query.value) {
-	    console.log('let publish in mqtt', request.query);
-            this.mqttClient.publish(request.query.topic, request.query.value);
+            console.log('let publish in mqtt', request.query);
+            let value = request.query.value;
+            if (value === 'off') {
+                value = "rf433 1 0101010101100110010101100110011001011010101001010\n"
+                      + "rf433 1 0101010101100110010101100110011010100101101001010\n"
+                      + "rf433 1 0101010101100110010101100110101001010101101001010\n"
+                      + "rf433 1 0101010101100101011001100110011001011010101001010\n"
+                      + "rf433 1 0101010101100101011001100110101001010101101001010\n"
+                      + "rf433 1 0101010101100101011001100110011010100101101001010\n";
+                value += value;
+            } else if (value === 'working') {
+                value = "rf433 1 0101010101100101011001100110011001011010010110100\n"
+                    + "rf433 1 0101010101100110010101100110101001010101010110100\n"
+                    + "rf433 1 0101010101100101011001100110011010100101101001010\n"
+                    + "rf433 1 0101010101100110010101100110011001011010101001010\n"
+                    + "rf433 1 0101010101100110010101100110011010100101101001010\n"
+                    + "rf433 1 0101010101100101011001100110101001010101101001010\n";
+                value += value;
+            } else if (value === 'chillout') {
+                value = "rf433 1 0101010101100101011001100110101001010101010110100\n"
+                    + "rf433 1 0101010101100101011001100110011010100101010110100\n"
+                    + "rf433 1 0101010101100110010101100110011001011010101001010\n"
+                    + "rf433 1 0101010101100110010101100110011010100101101001010\n"
+                    + "rf433 1 0101010101100110010101100110101001010101101001010\n"
+                    + "rf433 1 0101010101100101011001100110011001011010101001010\n";
+                value += value;
+            }
+            this.mqttClient.publish(request.query.topic, value);
         }
         else if (body.request.intent) {
             let intent = body.request.intent;
